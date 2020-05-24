4e8119491aaf07b64e6b81c7409d737ced58b06e
jun-g <daemonfreaks@gmail.com>
2020-05-24 23:04:08 +0700

    py3 comliant and PEP8ify.

--- sitemap.py
+++ sitemap.py
@@ -1,15 +1,17 @@
+# coding: utf-8
 """
 sitemap.py
 
-This plugin generates a sitemaps.org compliant sitemap when accessed to "$base_url/sitemap.xml".
+This plugin generates a sitemaps.org compliant sitemap.xml when accessed
+to "$base_url/sitemap.xml".
 This format is supported by Google, Yahoo, MSN.
 See also http://www.sitemaps.org/
 """
 
-__author__ = "Junji NAKANISHI <jun-g@daemonfreaks.com>"
-__version__ = "0.1"
-__url__ = "http://www.daemonfreaks.com/dev/sitemap.py"
-__description__ = "Generate a sitemap xml."
+__author__ = 'Junji NAKANISHI <jun-g@daemonfreaks.com>'
+__version__ = 'version 0.1.20200524'
+__url__ = "https://github.com/daemonfreaks/pyblosxom-plugins"
+__description__ = "This is a PyBlosxom plugin that generates sitemap xml."
 
 import os.path
 from Pyblosxom import tools, entries
@@ -24,6 +26,7 @@ TEMPLATE_URL = """<url>
 <lastmod>%s</lastmod>
 </url>"""
 
+
 def cb_handle(args):
     request = args["request"]
     data = request.getData()
@@ -33,14 +36,15 @@ def cb_handle(args):
     if pyhttp["PATH_INFO"] != url:
         return 0
     response = request.getResponse()
-    response.addHeader("Content-type", "text/xml")
+    response.addHeader("Content-type", "application/xml")
     urls = []
     for fname in tools.Walk(request, config["datadir"]):
         entry = entries.fileentry.FileEntry(request, fname, config["datadir"])
-        url = "%s.html" % os.path.join(config["base_url"], entry.get("absolute_path"), entry.get("fn"))
+        url = "%s.html" % (os.path.join(config["base_url"],
+                                        entry.get("absolute_path"),
+                                        entry.get("fn")))
         urls.append(TEMPLATE_URL % (url, entry.get("w3cdate")))
     urls.reverse()
     sitemap = TEMPLATE_BASE % ("\n".join(urls))
-    print >> response, sitemap
+    response.write(sitemap)
     return 1
-
