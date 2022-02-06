cafe559bae58a776c8d4c188e112c9f3b1f3a267
Junji NAKANISHI <daemonmfreaks@gmail.com>
2022-02-06 17:44:38 +0900

    Updated entrymtime.py

--- entrymtime.py
+++ entrymtime.py
@@ -5,9 +5,9 @@ entrymtime.py
 This plugin provides to overwrite entry's mtime using metadata.
 """
 
-__author__ = 'Junji NAKANISHI <jun-g@daemonfreaks.com>'
-__version__ = '0.1.20220103'
-__url__ = 'https://github.com/daemonfreaks/pyblosxom-plugins'
+__author__ = "Junji NAKANISHI <jun-g@daemonfreaks.com>"
+__version__ = "0.1.20220103"
+__url__ = "https://github.com/daemonfreaks/pyblosxom-plugins"
 
 import os
 import re
@@ -15,31 +15,30 @@ import time
 
 try:
     from dateutil import parser
+
     _dateutil_is_installed = True
 except ModuleNotFoundError:
     _dateutil_is_installed = False
 
-_regexp = re.compile(r'\n#entrymtime\s+(.*)\n')
+_regexp = re.compile(r"\n#entrymtime\s+(.*)\n")
 
 
 def verify_installation(request):
     return _dateutil_is_installed
 
+
 def cb_filestat(args):
-    request = args['request']
+    request = args["request"]
     config = request.get_configuration()
-    filename = args['filename']
-    fullname = os.path.join(config['datadir'], filename)
-    with open(fullname, 'r') as f:
+    filename = args["filename"]
+    fullname = os.path.join(config["datadir"], filename)
+    with open(fullname, "r") as f:
         contents = f.read()
         m = _regexp.search(contents)
         if m:
-            try:
-                mtime = []
-                for i in args['mtime']:
-                    mtime.append(i)
-                mtime[8] = time.mktime(parser.parse(m.group(1)).timetuple())
-                args['mtime'] = tuple(mtime)
-            except:
-                pass
+            mtime = []
+            for i in args["mtime"]:
+                mtime.append(i)
+            mtime[8] = time.mktime(parser.parse(m.group(1)).timetuple())
+            args["mtime"] = tuple(mtime)
     return args
